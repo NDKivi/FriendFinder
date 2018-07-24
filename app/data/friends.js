1615c38ae96c8f097e6f5bc445d5ba9527d1bc5f
NDKivi <kivineal@outlook.com>
2018-07-23 19:32:25 -0500

    minimum working product

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 const friends = [
     {
-        "name": "Uno One Un",
-        "photo": "",
+        "name": "Mr. Rogers",
+        "photo": "https://www.insideedition.com/sites/default/files/styles/931x523/public/images/2018-03/032318-mrrogers-1280x720.jpg?itok=CR7Tp36E&h=c673cd1c",
         "scores": [
             1,
             1,
@@ -16,8 +16,8 @@ const friends = [
         ]
     },
     {
-        "name": "Cinco Five Cinq",
-        "photo": "",
+        "name": "Bob Ross",
+        "photo": "https://yt3.ggpht.com/a-/ACSszfH-TGySV8bcz1a_rF-2cS0XVvkDEaA27r6qwQ=s900-mo-c-c0xffffffff-rj-k-no",
         "scores": [
             5,
             5,
@@ -32,8 +32,8 @@ const friends = [
         ]
     },
     {
-        "name": "Tres Three Trois",
-        "photo": "",
+        "name": "Nick Cage",
+        "photo": "https://m.media-amazon.com/images/M/MV5BMTUzMDM4Nzk2MV5BMl5BanBnXkFtZTcwNTcwNjExOQ@@._V1_UY317_CR1,0,214,317_AL_.jpg",
         "scores": [
             3,
             3,
@@ -45,6 +45,38 @@ const friends = [
             3,
             3,
             3
+        ]
+    },
+    {
+        "name": "Nickelback",
+        "photo": "https://cps-static.rovicorp.com/3/JPG_400/MI0003/458/MI0003458556.jpg?partner=allrovi.com",
+        "scores": [
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2
+        ]
+    },
+    {
+        "name": "Rick Astley",
+        "photo": "http://images.mentalfloss.com/sites/default/files/styles/mf_image_16x9/public/rickrollheader.png?itok=f_UDp9E2&resize=1100x619",
+        "scores": [
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            4
         ]
     }
 ];
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,14 +6,42 @@ function addAPIRoutes(app, friends, callback) {
 
     app.post("/api/friends", function (req, res) {
         let newFriend = req.body;
-
-
-        res.json(newFriend);
+        console.log(newFriend);
+        let bestFriend = getMatch(newFriend, friends);
+        res.json(bestFriend);
     });
 
     console.log("API Routes added");
     
     callback(app);
+}
+
+
+function getMatch(newFriend, friends) {
+    let closestMatch = friends[0];
+    let lowestScore = getScore(newFriend, friends[0]);
+    let currentScore;
+
+    for (let i = 1; i < friends.length; i++) {
+        if (newFriend.name !== friends[i].name) 
+            currentScore = getScore(newFriend, friends[i]);
+            if (currentScore < lowestScore) {
+                lowestScore = currentScore;
+                closestMatch = friends[i];
+            }
+    }
+    return closestMatch;
+}
+
+function getScore(friendOne, friendTwo) {
+    let arrayOne = friendOne.scores;
+    let arrayTwo = friendTwo.scores;
+    let score = 0;
+    for (let i = 0; i < arrayOne.length; i++) {
+        score += Math.abs(parseInt(arrayOne[i]) - parseInt(arrayTwo[i]));
+    }
+    console.log(score);
+    return score;
 }
 
 module.exports = addAPIRoutes;
